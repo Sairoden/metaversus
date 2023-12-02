88de94955736d7a8ad0b88e0879aa871a71c423f
Sairoden <odingandarosa@gmail.com>
2023-12-02 19:23:21 +0800

    Finish Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,6 +27,11 @@ function Navbar() {
         <h2 className="font-extrabold text-[24px] leading-[30px] text-white">
           METAVERSUS
         </h2>
+        <img
+          src="/menu.svg"
+          alt="menu icon"
+          className="w-[24px] h-[24px] object-contain"
+        />
       </div>
     </motion.nav>
   );

cbb5946ab931213b53cdb6ff30850e5649def8bc
Sairoden <odingandarosa@gmail.com>
2023-12-03 15:39:28 +0800

    Finish Explore and ExploreCard

--- src/sections/Explore.jsx
+++ src/sections/Explore.jsx
@@ -1,5 +1,55 @@
+// REACT & LIBRARIES
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+// COMPONENTS
+import { TypingText, ExploreCard, TitleText } from "../components";
+
+// STYLES
+import styles from "../styles";
+
+// CONSTANTS
+import { exploreWorlds } from "../constants";
+
+// UTILITIES
+import { fadeIn, staggerContainer } from "../utils";
+
 function Explore() {
-  return <section>Explore</section>;
+  const [active, setActive] = useState("world-2");
+
+  return (
+    <section className={`${styles.paddings}`} id="explore">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TypingText title="| The World" textStyles="text-center" />
+        <TitleText
+          title={
+            <>
+              Choose the world you want <br className="md:block hidden" /> to
+              explore
+            </>
+          }
+          textStyles="text-center"
+        />
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+          {exploreWorlds.map((world, index) => (
+            <ExploreCard
+              key={world.id}
+              {...world}
+              index={index}
+              active={active}
+              onClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
 }
 
 export default Explore;
